759b233327a4f6407dba16d3ad30c76df74bf3e4
Yifu Duan <yifu.duan@shopee.com>
2020-07-17 21:40:00 +0800

    delete target/

--- src/main/scala/ch1/Cat.scala
+++
@@ -1,8 +0,0 @@
-package ch1
-
-final case class Cat (name: String, age:Int, color:String)
-
-object jj{
-  List
-}
-
--- src/test/java/TestFunctor.java
+++
@@ -1,8 +0,0 @@
-import java.util.Optional;
-
-public class TestFunctor {
-    public static void main(String[] args) {
-        var t = Optional.empty().map(n -> "hi, " + n);
-
-    }
-}
